8e89ecd1de2d847be1cba7d60e1ef721a24412ad
Lia Zapciroiu <lzapciroiu@deloittece.com>
2020-05-06 18:50:40 +0300

    feature/build basic navigation based on sections

--- js/app.js
+++ js/app.js
@@ -4,53 +4,71 @@
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
 */
 
-/**
- * Define Global Variables
- *
-*/
+/* Global Variables */
 
+// DOM elements
+const allSections = document.querySelectorAll('[data-nav]'); //nodeList
+const landingContainers = document.getElementsByClassName('landing__container'); //HTMLCollection
+const navbarList = document.getElementById('navbar__list');
+// Storing data arrays
+let sectionsIds = [];
+let sectionsNames = [];
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
+/* Helper Functions */
+const getSectionsIds = () => {
+  allSections.forEach((section) => {
+    let sectionId = section.getAttribute('id');
+    sectionsIds.push(sectionId);
+  }); 
+  return sectionsIds;
+}
 
+const getSectionsNames = () => {
+  allSections.forEach((section) => {
+    let name = section.getAttribute('data-nav');
+    sectionsNames.push(name);
+  });
+  return sectionsNames;
+}
 
+getSectionsIds();
+getSectionsNames();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
+/* Main Functions */
 
 // build the nav
-
+const buildNavbar = () => {
+  const fragment = document.createDocumentFragment();
+  allSections.forEach((section, index) => {
+    // build an li
+    const newListItem = document.createElement('li');
+    const newAnchorTag = document.createElement('a');
+    newAnchorTag.setAttribute('href', `#${sectionsIds[index]}`);
+    newAnchorTag.classList.add('menu__link');
+    newAnchorTag.innerText = `${sectionsNames[index]}`;
+    // append the lis
+    newListItem.appendChild(newAnchorTag);
+    fragment.appendChild(newListItem);
+  });
+  navbarList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
-
 
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+/* Events */
 
 // Build menu 
+window.addEventListener('DOMContentLoaded', (event) => {
+  buildNavbar();
+});
 
 // Scroll to section on link click
+
 
 // Set sections as active
 
