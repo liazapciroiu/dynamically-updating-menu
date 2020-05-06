935ef30dd31eebd1ecefe138c8159918d5ee3cc9
Lia Zapciroiu <lzapciroiu@deloittece.com>
2020-05-06 23:09:50 +0300

    feature/ added smooth scrolling, highlight the active section

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
-/**
- *
- * Manipulating the DOM exercise.
+/* Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
-*/
+ * and highlights section in viewport upon scrolling. */
 
 /* Global Variables */
-
 // DOM elements
 const allSections = document.querySelectorAll('[data-nav]'); //nodeList
-const landingContainers = document.getElementsByClassName('landing__container'); //HTMLCollection
 const navbarList = document.getElementById('navbar__list');
 // Storing data arrays
 let sectionsIds = [];
 let sectionsNames = [];
 
 /* Helper Functions */
+const forEachSectionCallback = callback => {
+  allSections.forEach(callback);
+};
+
 const getSectionsIds = () => {
-  allSections.forEach((section) => {
+  forEachSectionCallback(section => {
     let sectionId = section.getAttribute('id');
     sectionsIds.push(sectionId);
-  }); 
+  });
   return sectionsIds;
-}
+};
 
 const getSectionsNames = () => {
-  allSections.forEach((section) => {
+  forEachSectionCallback(section => {
     let name = section.getAttribute('data-nav');
     sectionsNames.push(name);
   });
   return sectionsNames;
-}
+};
 
 getSectionsIds();
 getSectionsNames();
@@ -41,7 +40,7 @@ getSectionsNames();
 // build the nav
 const buildNavbar = () => {
   const fragment = document.createDocumentFragment();
-  allSections.forEach((section, index) => {
+  forEachSectionCallback((section, index) => {
     // build an li
     const newListItem = document.createElement('li');
     const newAnchorTag = document.createElement('a');
@@ -53,23 +52,40 @@ const buildNavbar = () => {
     fragment.appendChild(newListItem);
   });
   navbarList.appendChild(fragment);
+};
+
+// Add class 'active' to section when in viewport
+function toggleActiveClassSection() {
+  window.addEventListener('scroll', () => {
+    forEachSectionCallback((section) => {
+      let distanceFromTop = section.getBoundingClientRect().top;
+      let distanceFromBottom = section.getBoundingClientRect().bottom;
+      if (distanceFromTop <= 130 && distanceFromBottom >= 130) {
+        section.classList.add('your-active-class');
+      } else {
+        section.classList.remove('your-active-class');
+      }
+    });
+  });
 }
 
-// Add class 'active' to section when near top of viewport
-
 // Scroll to anchor ID using scrollTO event
-
+function smoothScrollFromNavbar() {
+  navbarList.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.nodeName === 'A') {
+      const selectedSection = document.querySelector(event.target.getAttribute('href'));
+      selectedSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  })
+}
 
 /* Events */
-
-// Build menu 
-window.addEventListener('DOMContentLoaded', (event) => {
+// Build menu, toggle active class, handle scroll
+window.addEventListener('DOMContentLoaded', () => {
   buildNavbar();
+  toggleActiveClassSection();
+  smoothScrollFromNavbar();
 });
-
-// Scroll to section on link click
-
-
-// Set sections as active
-
-
